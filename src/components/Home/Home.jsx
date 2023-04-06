f4f5f15d0ea22599940ec94425ead55c246bd41f
almahmudsarker <almahmuds427@gmail.com>
2023-04-07 02:09:57 +0600

    cart&t-shirt  added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
-
+import TShirt from '../TShirt/TShirt';
+import Cart from '../Cart/Cart';
+import './Home.css';
+import { toast } from 'react-hot-toast';
 const Home = () => {
     const tshirts = useLoaderData();
+    const [cart, setCart] = useState([]);
+
+    const handleAddToCart = (tshirt) => {
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if(exists){
+            toast('T-Shirt already added to cart')
+        }else{
+            const newCart = [...cart, tshirt];
+            setCart(newCart);
+        }
+    }
+
+    const handleRemoveFromCart = (id) => {
+        const remaining = cart.filter(ts => ts._id !== id);
+        setCart(remaining);
+    }
+
     return (
-        <div>
-            <h1>This is Home: {tshirts.length}</h1>
+        <div className='home-container'>
+            <div className='t-shirts-container'>
+            {
+                tshirts.map(tshirt=> <TShirt
+                key={tshirt._id}
+                tshirt={tshirt}
+                handleAddToCart={handleAddToCart}
+                ></TShirt>)
+            }
+            </div>
+            <div className="cart-container">
+                <Cart cart={cart}
+                handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
+            </div>
         </div>
     );
 };
--- src/components/Layout/Main.jsx
+++ src/components/Layout/Main.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Toaster toastOptions={{
+                      className: '',
+                        style: {
+                          border: '1px solid #7132',
+                          color: 'gray',
+                         },
+                    }} />
         </div>
     );
 };
